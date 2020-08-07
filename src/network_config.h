b8ac9b341cc1d05773e60e2cd02e2eb1a36dcee9
robbinl <robbinl@telusplanet.net>
2020-08-07 15:25:49 -0600

    a

--- src/network_config.h
+++ src/network_config.h
@@ -8,8 +8,11 @@
 #define IO_KEY         "c10ce8eff2ad45c499dd864629ae40e2"
 
 /******************************* WIFI **************************************/
-#define WIFI_SSID       "ardwifi"
-#define WIFI_PASS       "ardwifi987"
+#define WIFI_SSID       "OldRob"
+#define WIFI_PASS       "5223qaz7542PLM"
 
-#include "AdafruitIO_WiFi.h"   // Beware, at the time of writting this, the Adafruit library needs platform-espressif32 version 1.7.0, https://github.com/platformio/platform-espressif32/releases, specified in platform.io
+#include "AdafruitIO_WiFi.h"   
+// Beware, at the time of writting this, 
+// the Adafruit library needs platform-espressif32 
+// version 1.7.0, https://github.com/platformio/platform-espressif32/releases, specified in platform.io
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
